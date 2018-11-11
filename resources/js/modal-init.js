a9b8a6a67cf4105a16b4e5a0eedb9595a4145897
Sammii-HK <sammii.h@icloud.com>
2018-11-11 21:28:35 +0000

    file update

--- resources/js/modal-init.js
+++ resources/js/modal-init.js
@@ -1,9 +1,20 @@
 // JavaScript Document
 
-(function(){
+/*(function(){
   var $content = $(this.'.modal-content').detach();   // Remove modal from page
 
   $(this.'.item-content').on('click', function() {           // Click handler to open modal
     modal.open({content: $content, width:340, height:300});
   });
-}());
+}());*/
+
+ $(".modal").click(function(){
+        var _button = $(this).attr('id');
+        var isOpen = $("."+_button+"_modal").css('display') === 'block';
+        if(isOpen){
+            $("."+_button+"_modal").css('display', 'none');
+        }else{
+            $("."+_button+"_modal").css('display', 'block');
+        }
+
+    })
